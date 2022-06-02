cda9f3b94a14cfebefdca94901291dcfb5a78fb4
Jingyi Mao <emmamao1022@gmail.com>
2022-06-02 14:04:53 +0000

    Add self-introduction and photo.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,19 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Shows a random quotes to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
+function addRandomQuote() {
+    const quotes =
+        ['You talking to me? - Taxi Driver, 1976', 
+        'To infinity and beyond! -Toy Story, 1995', 
+        'Life is a banquet, and most poor suckers are starving to death! -Auntie Mame, 1958', 
+        'Jessica, only child, Illinois, Chicago. -Parasite, 2019'];
+  
+    // Pick a random quote.
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+  
+    // Add it to the page.
+    const quoteContainer = document.getElementById('quote-container');
+    quoteContainer.innerText = quote;
+  }
