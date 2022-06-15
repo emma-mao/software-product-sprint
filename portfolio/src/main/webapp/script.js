bd042316f130101ca1e263cafcfbf367e2e3b066
Jingyi Mao <emmamao1022@gmail.com>
2022-06-15 03:53:21 +0000

    add translation feature

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -43,3 +43,23 @@ async function addRandomQuote() {
    dateContainer.innerText = textFromResponse;
  }
  
+ // for translation
+function requestTranslation() {
+    const text = document.getElementById('text').value;
+    const languageCode = document.getElementById('language').value;
+  
+    const resultContainer = document.getElementById('result');
+    resultContainer.innerText = 'Loading...';
+  
+    const params = new URLSearchParams();
+    params.append('text', text);
+    params.append('languageCode', languageCode);
+  
+    fetch('/translate', {
+      method: 'POST',
+      body: params
+    }).then(response => response.text())
+    .then((translatedMessage) => {
+      resultContainer.innerText = translatedMessage;
+    });
+  }
